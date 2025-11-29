33a47429fe37c45de068615aa0599e657e747a85
crocchi <ziocro@gmail.com>
2025-11-29 16:31:27 +0100

    up

--- .devcontainer/config.js
+++ .devcontainer/config.js
@@ -16,7 +16,7 @@ const assistantId = process.env.ASSISTANT_ID;
 if (!assistantId) {
   throw new Error("L'ID dell'assistente non è stato trovato nel file .env (ASSISTANT_ID)");
 }
-export const vectorStoreId = process.env.VECTOR_STORE_ID; 
+const vectorStoreId = process.env.VECTOR_STORE_ID; 
 
 // --- CONFIGURAZIONE TELEGRAM ---
 const token = process.env.TELEGRAM_BOT_TOKEN;
