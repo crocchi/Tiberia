73b786f1fb023d2ca764363613144d3260c76ee5
crocchi <ziocro@gmail.com>
2025-12-05 00:32:20 +0100

    Update voice parameter in startModelAudio function to use 'fable'

--- utility/modelEvent.js
+++ utility/modelEvent.js
@@ -6,7 +6,7 @@ export const startModelAudio = async (responseText) => {
 
 const mp3 = await client.audio.speech.create({
             model: "gpt-4o-mini-tts",//"tts-1",gpt-realtime-mini
-            voice: "shale", // Puoi scegliere tra: alloy, echo, fable, onyx, nova, shale
+            voice: "fable", // Puoi scegliere tra: alloy, echo, fable, onyx, nova, shale
             input: 'Ciao io sono Spongebob, e tu? COme stai in questa bellissima giornata? Spero tutto bene!',
             instructions: "Parla con la voce simile a spongebob ",
           });
