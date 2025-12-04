79e71e645285221e8b62c7b31dc7ffa56e04aa21
crocchi <ziocro@gmail.com>
2025-12-04 21:23:51 +0100

    up

--- utility/modelEvent.js
+++ utility/modelEvent.js
@@ -6,8 +6,8 @@ export const startModelAudio = async (responseText) => {
 
 const mp3 = await client.audio.speech.create({
             model: "gpt-4o-mini-tts",//"tts-1",gpt-realtime-mini
-            voice: "alloy", // Puoi scegliere tra: alloy, echo, fable, onyx, nova, shale
-            input: 'mi fai una piccola introduzione audio, devi dire..:Ciao io sono Spongebob, e tu? COme stai in questa bellissima giornata? Spero tutto bene!',
+            voice: "shale", // Puoi scegliere tra: alloy, echo, fable, onyx, nova, shale
+            input: 'Ciao io sono Spongebob, e tu? COme stai in questa bellissima giornata? Spero tutto bene!',
             instructions: "Parla con la voce simile a spongebob ",
           });
           const audioBuffer = Buffer.from(await mp3.arrayBuffer());
