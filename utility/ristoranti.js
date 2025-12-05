3340546556fc10ec8c8a4384b00ffb4212f7ab63
crocchi <ziocro@gmail.com>
2025-12-06 00:59:41 +0100

    ui

--- utility/ristoranti.js
+++ utility/ristoranti.js
@@ -73,5 +73,6 @@ export async function getOpenRestaurantsCapri() {
         openingHours: place.openingHours
     }));*/
     console.log("Ristoranti aperti a Capri:", JSON.stringify(data));
+    console.log(data.map(place => place.displayName));
     return data;
 }
