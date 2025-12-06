c987d216655351a75eb5161a278bb140fb76e58b
crocchi <ziocro@gmail.com>
2025-12-06 03:14:12 +0100

    ss

--- index.js
+++ index.js
@@ -68,12 +68,12 @@ app.use('/models', express.static(path.join(process.cwd(), 'models')));
 function startEventsUpdater() {
     console.log('Avvio del servizio di aggiornamento eventi di Capri.');
     //aggionra le news per queste date
-    const updateStart='2025/12/01';
-    const updateEnd='2025/12/02';
+    const updateStart='2025/12/02';
+    const updateEnd='2025/12/06';
 
     // inizializza il DB VECTOR startup
-   //  fetchAndIndexEvents([3,4,5,7],INDEX_DB_NEWS,updateStart,updateEnd,80);//init DB
-    //fetchAndIndexEvents([6], INDEX_DB_EVENTS, updateStart,updateEnd,100);//init DB
+    fetchAndIndexEvents([3,4,5,7],INDEX_DB_NEWS,updateStart,updateEnd,80);//init DB
+    fetchAndIndexEvents([6], INDEX_DB_EVENTS, updateStart,updateEnd,100);//init DB
     //fetchAndIndexEvents(categorie scelte,database name,data start, data end, quantita);//init DB eventi odierni
 
     // Carica i thread esistenti dal DB vettoriale
